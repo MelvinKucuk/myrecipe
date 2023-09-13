95333628fd61830a46c5483e2f7c17e85d13bce6
Melvin <melvinalek@gmail.com>
2023-09-13 18:52:27 -0300

    Retrofit setUp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,8 @@ android {
     namespace = "com.melvin.myrecipe"
     compileSdk = 33
 
+    android.buildFeatures.buildConfig = true
+
     defaultConfig {
         applicationId = "com.melvin.myrecipe"
         minSdk = 26
@@ -20,6 +22,8 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        buildConfigField("String", "BASE_URL", "\"http://demo2328879.mockable.io/\"")
     }
 
     buildTypes {
@@ -53,8 +57,13 @@ android {
 
 dependencies {
 
+    //Core
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
 
     //UI
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -69,6 +78,12 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.46.1")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 
     //Unit Test
     testImplementation("junit:junit:4.13.2")
